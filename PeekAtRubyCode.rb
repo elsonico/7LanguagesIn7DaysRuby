1e253195d37da9ca1d9c940735e057eddd8a4f1f
Sonico Elephas <vaattanen@gmail.com>
2016-08-04 17:37:10 -0400

    Closing the day

--- PeekAtRubyCode.rb
+++ PeekAtRubyCode.rb
@@ -6,3 +6,21 @@ properties.each { |property| puts "Ruby is #{property}."}
 #Ruby is duck typed.
 #Ruby is productive.
 #Ruby is fun.
+  
+#[tav@inhambane ~]$ irb
+#irb(main):001:0> puts 'Hello, world'
+#Hello, world
+#=> nil
+#[tav@inhambane ~]$ irb
+#irb(main):001:0> puts 'Hello, world'
+#Hello, world
+#=> nil
+#irb(main):004:0> puts "hello, #{language}"
+#hello, Ruby
+#=> nil
+#irb(main):006:0> language = 'my Ruby'
+#=> "my Ruby"
+#irb(main):007:0> puts "hello, #{language}"
+#hello, my Ruby
+#=> nil
+
