4ec06ecb8c44f5698567b4db27bf06a695968806
Sonico Elephas <vaattanen@gmail.com>
2016-08-05 13:33:06 -0400

    Updated PickRandomNumber.py

--- PickRandomNumber.rb
+++ PickRandomNumber.rb
@@ -1,10 +1,10 @@
-#!/usr/bin/env ruby
-
-$stdout.print 'Pick a random number between 0 and 9? '
-number = $stdin.readline()
-random = rand(10)
-if  number.to_i == random
-  puts "Lucky you you guessed right, the winning number was #{random}!"
-else
-  puts "Sorry but correct number was #{random}."
+class PickRandomNumber  
+  $stdout.print 'Pick a ran4dom number between 0 and 9? '
+  number = $stdin.readline()
+  random = rand(10)
+  if  number.to_i == random
+    puts "Lucky you you guessed right, the winning number was #{random}!"
+  else
+    puts "Sorry but correct number was #{random}."
+  end
 end
