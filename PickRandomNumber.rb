9bc8d0582ab9b416b43247b027d75e5bc80d8975
Tapio Vaattanen <vaattanen@gmail.com>
2016-08-05 12:16:04 -0400

    Update PickRandomNumber.rb

--- PickRandomNumber.rb
+++ PickRandomNumber.rb
@@ -4,7 +4,7 @@ $stdout.print 'Pick a random number between 0 and 9? '
 number = $stdin.readline()
 random = rand(10)
 if  number.to_i == random
-  puts 'Lucky you you guessed right!'
+  puts 'Lucky you you guessed right, the winning number was #{random}!'
 else
   puts "Sorry but correct number was #{random}."
 end
